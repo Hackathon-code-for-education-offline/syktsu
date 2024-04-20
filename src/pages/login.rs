b88ec229569eaa98c0db2fcd27c5dde51b32f292
Spleit <1spleit171@gmail.com>
2024-04-20 19:53:01 +0300

    modify login page

--- src/pages/login.rs
+++ src/pages/login.rs
@@ -3,8 +3,12 @@ use yew::prelude::*;
 #[function_component(Login)]
 pub fn login() -> Html {
     html! {
-        <div>
-            { id }
+        <div class={classes!("login__form")}>
+            <span class={classes!("login__form__logo")}></span>
+
+            <div class={classes!("login__form__title")}>
+            </div>
+
         </div>
     }
 }
