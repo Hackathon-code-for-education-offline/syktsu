65ff132d11260b680370cca2ea030bf31000189c
instanc3 <mail.instanc3@gmail.com>
2024-04-20 16:27:42 +0300

    Добавил сериализацию для типов

--- interface/src/lib.rs
+++ interface/src/lib.rs
@@ -1,6 +1,8 @@
 use std::borrow::Cow;
 
-#[derive(Clone, Debug)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct LoginRequest<'a> {
     username: Cow<'a, str>,
     password: Cow<'a, str>,
