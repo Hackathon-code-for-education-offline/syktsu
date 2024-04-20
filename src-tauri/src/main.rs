adcc2fef5412fef7a73b1fd748970aeb028cc257
Spleit <1spleit171@gmail.com>
2024-04-20 17:15:17 +0300

    set tokio runtime

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,20 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod db;
 mod handlers;
 mod state;
-mod db;
 
-use state::State;
 use handlers::example;
+use state::State;
 
 #[tokio::main]
 async fn main() {
+    tauri::async_runtime::set(tokio::runtime::Handle::current());
+
     tauri::Builder::default()
         .manage(State::init().await.unwrap())
-        .invoke_handler(tauri::generate_handler![
-            example
-        ])
+        .invoke_handler(tauri::generate_handler![example])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
