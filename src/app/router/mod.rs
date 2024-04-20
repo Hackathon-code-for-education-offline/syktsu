722354afd9b63955c92ef43113852c27188b77e2
Spleit <1spleit171@gmail.com>
2024-04-20 14:23:42 +0300

    add Profile and University path

--- src/app/router/mod.rs
+++ src/app/router/mod.rs
@@ -1,15 +1,23 @@
-use yew_router::prelude::*;
+use crate::pages::{
+    home::Home,
+    profile::{MyProfile, Profile},
+    university::{Universities, University},
+};
 use yew::prelude::*;
-use crate::pages::home::Home;
+use yew_router::prelude::*;
 
 #[derive(Debug, Clone, PartialEq, Routable)]
 pub enum Route {
     #[at("/")]
     Home,
+    #[at("/profile")]
+    ProfileRoot,
     #[at("/profile/:id")]
-    Profile {
-        id: String
-    },
+    Profile { id: String },
+    #[at("/university")]
+    UniversityRoot,
+    #[at("/university/:id")]
+    University { id: String },
     #[not_found]
     #[at("/404")]
     NotFound,
@@ -37,7 +45,11 @@ pub(crate) fn app() -> Html {
 fn switch(routes: Route) -> Html {
     match routes {
         Route::Home => html! { <Home /> },
+        Route::UniversityRoot => html! { <Universities /> },
+        Route::University { id } => html! { <University {id} /> },
+        Route::ProfileRoot => html! { <MyProfile /> },
+        Route::Profile { id } => html! { <Profile {id} /> },
+
         Route::NotFound => html! { <h1>{ "404" }</h1> },
-        _ => html! { <Redirect<Route> to={Route::NotFound} /> }
     }
 }
--- src/pages/profile.rs
+++ src/pages/profile.rs
@@ -2,16 +2,25 @@ use yew::prelude::*;
 
 #[derive(PartialEq, Properties)]
 pub struct ProfileProps {
-    id: AttrValue
+    pub id: AttrValue,
 }
 
-#[function_component]
-pub fn ComponentName(props: &ProfileProps) -> Html {
+#[function_component(Profile)]
+pub fn profile(props: &ProfileProps) -> Html {
     let ProfileProps { id } = props;
 
     html! {
         <div>
             { id }
+        </div>
+    }
+}
+
+#[function_component(MyProfile)]
+pub fn my_profile() -> Html {
+    html! {
+        <div>
+            { "My profile" }
         </div>
     }
 }
