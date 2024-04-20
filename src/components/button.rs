9122abe435945d1badbbe9bc95e3236eb9123c81
Spleit <1spleit171@gmail.com>
2024-04-20 19:53:12 +0300

    add button component

---
+++ src/components/button.rs
@@ -0,0 +1,32 @@
+use yew::prelude::*;
+
+#[derive(PartialEq, Properties)]
+pub struct ButtonProps {
+    #[prop_or_default]
+    pub is_accent: bool,
+
+    #[prop_or_default]
+    pub with_icon: Option<AttrValue>,
+
+    #[prop_or_default]
+    pub value: Option<AttrValue>,
+}
+
+#[function_component(Button)]
+pub fn button(props: &ButtonProps) -> Html {
+    let ButtonProps {
+        is_accent,
+        with_icon,
+        value,
+    } = props;
+
+    html! {
+        <div class={classes!("button", if *is_accent {"accent"} else {"base"})}>
+            if let Some(icon_name) = with_icon {
+                <span class={classes!(icon_name)}></span>
+            }
+
+            { value.clone() }
+        </div>
+    }
+}
