f9d7a8a7a42499f16b048bdad603a8613cad4b69
instanc3 <mail.instanc3@gmail.com>
2024-04-20 17:16:58 +0300

    Убрал лишний код

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,7 +8,6 @@ async fn main() {
     tauri::async_runtime::set(tokio::runtime::Handle::current());
 
     tauri::Builder::default()
-        .manage(State::init().await.unwrap())
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
