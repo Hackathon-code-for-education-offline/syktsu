6931997672e2e870d8918ecdf368e65ede3442db
instanc3 <mail.instanc3@gmail.com>
2024-04-20 20:50:06 +0300

    Добавил создание необходимых таблиц базы данных

--- infrastructure/auth_server/src/db.rs
+++ infrastructure/auth_server/src/db.rs
@@ -1,8 +1,10 @@
-use sqlx::{mysql::MySqlPoolOptions, query, MySql, Pool};
+use sqlx::{mysql::MySqlPoolOptions, query, MySql, MySqlPool, Pool};
+
 
 const DB_NAME: &str = "test_db";
 const DB_USER: &str = "test_user";
 const DB_PASS: &str = "test_pass";
+const ROOT_PASS: &str = "example";
 
 pub trait Data {}
 
@@ -24,13 +26,15 @@ impl User {
 impl Data for User {}
 
 pub struct DB {
-    pool: Pool<MySql>
+    pub pool: Pool<MySql>
 }
 
 impl DB {
     pub async fn init() -> Self {
-        let url = format!("mysql://{DB_USER}:{DB_PASS}@localhost:4444/{DB_NAME}");
-        let pool = MySqlPoolOptions::new().connect(&url).await.unwrap();
+        let url = format!("mysql://root:{ROOT_PASS}@localhost:3306/{DB_NAME}");
+        let pool = MySqlPool::connect(&url).await.unwrap();
+
+        println!("Успешно подключен к базе данных!");
         
         Self { 
             pool 
@@ -41,8 +45,7 @@ impl DB {
         query(&format!("SELECT * FROM {};", name)).execute(&self.pool).await.unwrap();
     }
 
-    pub async fn insert(&self, name: &str, values: User) 
-    {
+    pub async fn insert(&self, name: &str, values: User) {
         query(&format!(r"INSERT INTO {} VALUES (
             {}, {}
         );", 
@@ -53,18 +56,224 @@ impl DB {
     }
 
     pub async fn create(&self, name: &str) {
-        query(&format!(r"CREATE TABLE IF NOT EXIST {} (
-            id int,
-            name text,
-        );", name)).execute(&self.pool).await.unwrap();
-    }
-
-    pub async fn select(&self) {
-        query(r"").execute(&self.pool).await.unwrap();
-    }
-    
-    pub async fn delete(&self) {
-        query(r"").execute(&self.pool).await.unwrap();
+        query(&format!(r#"CREATE TABLE IF NOT EXIST {} (
+            id int, 
+            name text
+        );"#, name)).execute(&self.pool).await.unwrap();
     }
 }
 
+pub struct Tables {
+    university: University,
+    users: Users,
+
+}
+
+impl Tables {
+
+}
+
+enum Roles {
+    Guest,
+    Student,
+    Admin,
+}
+
+
+struct University {
+    id: i64,
+    title: String,
+    city: String,
+}
+
+impl University {
+    async fn create(pool: Pool<MySql>) {
+        if let Err(e) = query(r#"CREATE TABLE University (
+            university_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+            title VARCHAR(256) NOT NULL,
+            city VARCHAR(64),
+               admission_committee VARCHAR(512)
+            );"#).execute(&pool).await 
+        {
+            println!("{:?}", e);
+        };
+    }
+
+    async fn insert(pool: Pool<MySql>) {
+        if let Err(e) = query(r#""#).execute(&pool).await {
+            println!("{:?}", e);
+        };
+    }
+}
+
+struct Users {
+    id: i64,
+    univer_id: i64,
+    first_name: String,
+    last_name: String,
+    middle_name: String,
+    role: Roles,
+    institute: String,
+    phone: String,
+}
+
+impl Users {
+    async fn create(pool: Pool<MySql>) {
+        if let Err(e) = query(r#"CREATE TABLE Users (
+            user_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+            university_id INTEGER NOT NULL,
+            last_name VARCHAR(128),
+            first_name VARCHAR(128),
+            middle_name VARCHAR(128),
+            `role` VARCHAR(128) NOT NULL,
+            institute VARCHAR(128),
+            phone VARCHAR(16),
+               FOREIGN KEY(university_id) REFERENCES University(university_id)
+            );"#).execute(&pool).await 
+        {
+            println!("{:?}", e);
+        };
+    }
+
+    async fn insert(pool: Pool<MySql>) {
+        if let Err(e) = query(r#""#).execute(&pool).await {
+            println!("{:?}", e);
+        };
+    }
+}
+
+struct UserRoles {
+    id: i64,
+    role: Roles,
+}
+
+impl UserRoles {
+    async fn create(pool: Pool<MySql>) {
+        if let Err(e) = query(r#"CREATE TABLE UsersRoles (
+            user_id INTEGER NOT NULL,
+            `role` VARCHAR(64) NOT NULL,
+            PRIMARY KEY(user_id, `role`),
+            FOREIGN KEY (user_id) REFERENCES Users(user_id),
+            FOREIGN KEY (`role`) REFERENCES Roles(`role`)
+            );"#).execute(&pool).await 
+        {
+            println!("{:?}", e);
+        };
+    }
+
+    async fn insert(pool: Pool<MySql>) {
+        if let Err(e) = query(r#""#).execute(&pool).await {
+            println!("{:?}", e);
+        };
+    }
+}
+
+struct Reviews {
+    id: i64,
+    user_id: i64,
+    review: String,
+    date_time: usize,
+    evaluation: i64,
+}
+
+impl Reviews {
+    async fn create(pool: Pool<MySql>) {
+        if let Err(e) = query(r#"CREATE TABLE Reviews (
+            review_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+            user_id INTEGER NOT NULL,
+            review TEXT NOT NULL,
+            date_time DATETIME,
+            evaluation INTEGER,
+               FOREIGN KEY(user_id) REFERENCES Users(user_id)
+           );"#).execute(&pool).await 
+        {
+            println!("{:?}", e);
+        };
+    }
+
+    async fn insert(pool: Pool<MySql>) {
+        if let Err(e) = query(r#""#).execute(&pool).await {
+            println!("{:?}", e);
+        };
+    }
+}
+
+struct Comments {
+    user_id: i64,
+    review_id: i64,
+    date_time: usize,
+}
+
+impl Comments {
+    async fn create(pool: Pool<MySql>) {
+        if let Err(e) = query(r#"CREATE TABLE Comments (
+            user_id INTEGER NOT NULL,
+            review_id INTEGER NOT NULL,
+            `comment` TEXT NOT NULL,
+            date_time DATETIME,
+            PRIMARY KEY(user_id, review_id),
+            FOREIGN KEY(user_id) REFERENCES Users(user_id),
+            FOREIGN KEY(review_id) REFERENCES Reviews(review_id)
+            );"#).execute(&pool).await 
+        {
+            println!("{:?}", e);
+        };
+    }
+
+    async fn insert(pool: Pool<MySql>) {
+        if let Err(e) = query(r#""#).execute(&pool).await {
+            println!("{:?}", e);
+        };
+    }
+}
+
+struct Locations {
+    id: usize,
+    title: String,
+    info: String,
+}
+
+impl Locations {
+    async fn create(pool: Pool<MySql>) {
+        if let Err(e) = query(r#"CREATE TABLE Locations (
+            location_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+            university_id INTEGER NOT NULL,
+            title VARCHAR(128) NOT NULL,
+            information TEXT,
+            FOREIGN KEY(university_id) REFERENCES University(university_id)
+            );"#).execute(&pool).await {
+            println!("{:?}", e);
+        };
+    }
+
+    async fn insert(pool: Pool<MySql>) {
+        if let Err(e) = query(r#""#).execute(&pool).await {
+            println!("{:?}", e);
+        };
+    }
+}
+
+struct Pictures {
+    id: usize,
+    pointer: String,
+}
+
+impl Pictures {
+    async fn create(pool: Pool<MySql>) {
+        if let Err(e) = query(r#"CREATE TABLE Pictures (
+            picture_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+            location_id INTEGER NOT NULL,
+            pointer VARCHAR(512),
+            FOREIGN KEY(location_id) REFERENCES Locations(location_id)
+            );"#).execute(&pool).await 
+        {
+            println!("{:?}", e);
+        };
+    }
+
+    async fn insert(pool: Pool<MySql>) {
+        if let Err(e) = query(r#""#).execute(&pool).await {
+            println!("{:?}", e);
+        };
+    }
+}
--- infrastructure/auth_server/src/main.rs
+++ infrastructure/auth_server/src/main.rs
@@ -3,7 +3,12 @@
 mod db;
 
 use db::{User, DB};
-
+use rocket_db_pools::{
+    sqlx::{self, Row, MySqlPool},
+    Connection,
+    Database,
+};
+use sqlx::query;
 struct State {
     
 }
@@ -12,25 +17,40 @@ struct State {
 impl State {
     pub async fn init() {
         let db = DB::init().await;
+        println!("here!");
 
-        let table_name = "playground";
-    
-        db.create(table_name).await;
-        db.insert(table_name, User::new(1, format!("Stiven"))).await;
-        db.show_table(table_name).await;
+//         match query("CREATE TABLE University (university_id INTEGER PRIMARY KEY AUTO_INCREMENT, title VARCHAR(256) NOT NULL, city VARCHAR(64), admission_committee VARCHAR(512));").execute(&db.pool).await {
+//             Ok(_) => println!("Таблица University создана"),
+//             Err(e) => println!("{e}"),
+//         };
+
+//         query("INSERT INTO University(university_id, title) values (1, 'null');").execute(&db.pool).await.unwrap();
+        
+//         let r = query!(r#"
+// SELECT * 
+// FROM University
+//                         "#)
+//             .fetch_all(&db.pool).await.unwrap();
+
+//         // let r = query("SELECT * FROM University").execute(&db.pool).await.unwrap();
+//         println!("pinus {:?}", r);
+
+//         for res in r {
+//             println!("{}", res.university_id)
+//         }
+        
     }
 }
+
 
 #[launch]
 async fn rocket() -> _ {
     let state = State::init().await;
 
     rocket::build()
-    .manage(state)
-    .mount("/", routes![index])
+        .manage(state)
+        .mount("/", routes![
+            // read
+        ])
 }
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
