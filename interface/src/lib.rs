890a1cb64fcce6e950d40557051ecd294ac2ac01
Spleit <1spleit171@gmail.com>
2024-04-21 11:57:22 +0300

    nothing change

--- interface/src/lib.rs
+++ interface/src/lib.rs
@@ -4,6 +4,6 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct LoginRequest<'a> {
-    username: Cow<'a, str>,
-    password: Cow<'a, str>,
+    pub username: Cow<'a, str>,
+    pub password: Cow<'a, str>,
 }
