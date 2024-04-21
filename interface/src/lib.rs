4767eecc5ea7348ad99275dd3c21dbef9dc38281
instanc3 <mail.instanc3@gmail.com>
2024-04-21 15:17:38 +0300

    Добавил cors

--- interface/src/lib.rs
+++ interface/src/lib.rs
@@ -24,10 +24,16 @@ pub enum Code {
     InternalServerError,
 }
 
+// #[derive(Clone, Debug, Serialize, Deserialize, FromForm)]
+// pub struct LoginRequest<'a> {
+//     pub username: Cow<'a, str>,
+//     pub password: Cow<'a, str>,
+// }
+
 #[derive(Clone, Debug, Serialize, Deserialize, FromForm)]
-pub struct LoginRequest<'a> {
-    pub username: Cow<'a, str>,
-    pub password: Cow<'a, str>,
+pub struct LoginRequest {
+    pub username: String,
+    pub password: String,
 }
 
 #[derive(Debug)]
@@ -38,9 +44,46 @@ pub enum Error {
     Io(std::io::Error),
 }
 
+// #[cfg(feature = "backend")]
+// #[rocket::async_trait]
+// impl<'r> FromData<'r> for LoginRequest<'r> {
+//     type Error = Error;
+
+//     async fn from_data(req: &'r Request<'_>, data: Data<'r>) -> data::Outcome<'r, Self> {
+//         use Error::*;
+
+//         // Ensure the content type is correct before opening the data.
+//         let person_ct = ContentType::new("application", "x-person");
+//         if req.content_type() != Some(&person_ct) {
+//             return Outcome::Forward((data, Status::UnsupportedMediaType));
+//         }
+
+//         // Use a configured limit with name 'person' or fallback to default.
+//         let limit = req.limits().get("person").unwrap_or(256.bytes());
+
+//         // Read the data into a string.
+//         let string = match data.open(limit).into_string().await {
+//             Ok(string) if string.is_complete() => string.into_inner(),
+//             Ok(_) => return Outcome::Error((Status::PayloadTooLarge, TooLarge)),
+//             Err(e) => return Outcome::Error((Status::InternalServerError, Io(e))),
+//         };
+
+//         // We store `string` in request-local cache for long-lived borrows.
+//         let string = request::local_cache!(req, string);
+
+//         // Split the string into two pieces at ':'.
+//         let (username, password) = match string.find(':') {
+//             Some(i) => (&string[..i], &string[(i + 1)..]),
+//             None => return Outcome::Error((Status::UnprocessableEntity, NoColon)),
+//         };
+
+//         Outcome::Success(LoginRequest { username: Cow::Owned(format!("{}", username)), password: Cow::Owned(format!("{}", password)) })
+//     }
+// }
+
 #[cfg(feature = "backend")]
 #[rocket::async_trait]
-impl<'r> FromData<'r> for LoginRequest<'r> {
+impl<'r> FromData<'r> for LoginRequest {
     type Error = Error;
 
     async fn from_data(req: &'r Request<'_>, data: Data<'r>) -> data::Outcome<'r, Self> {
@@ -71,7 +114,7 @@ impl<'r> FromData<'r> for LoginRequest<'r> {
             None => return Outcome::Error((Status::UnprocessableEntity, NoColon)),
         };
 
-        Outcome::Success(LoginRequest { username: Cow::Owned(format!("{}", username)), password: Cow::Owned(format!("{}", password)) })
+        Outcome::Success(LoginRequest { username: format!("{}", username), password: format!("{}", password) })
     }
 }
 
