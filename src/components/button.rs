c547de8b2a83bae850c05500682c3b23890f027b
Spleit <1spleit171@gmail.com>
2024-04-21 11:58:14 +0300

    modify input and button components

--- src/components/button.rs
+++ src/components/button.rs
@@ -3,30 +3,46 @@ use yew::prelude::*;
 #[derive(PartialEq, Properties)]
 pub struct ButtonProps {
     #[prop_or_default]
+    pub _type: Option<AttrValue>,
+
+    #[prop_or_default]
     pub is_accent: bool,
 
     #[prop_or_default]
     pub with_icon: Option<AttrValue>,
 
     #[prop_or_default]
+    pub with_border: bool,
+
+    #[prop_or_default]
     pub value: Option<AttrValue>,
+
+    #[prop_or_default]
+    pub classes: Classes,
+
+    #[prop_or_default]
+    pub onclick: Callback<MouseEvent>,
 }
 
 #[function_component(Button)]
 pub fn button(props: &ButtonProps) -> Html {
     let ButtonProps {
+        _type,
         is_accent,
         with_icon,
         value,
+        onclick,
+        with_border,
+        classes,
     } = props;
 
     html! {
-        <div class={classes!("button", "text-size-m16", if *is_accent {"accent"} else {"base"})}>
+        <button type={_type.clone().unwrap_or("button".into())} class={classes!(classes.clone(), "button", "text-size-m16", if *is_accent {"accent"} else {"base"},  if *with_border {""} else {"no-border"})} {onclick}>
             if let Some(icon_name) = with_icon {
                 <span class={classes!(icon_name)}></span>
             }
 
             { value.clone() }
-        </div>
+        </button>
     }
 }
--- src/components/input.rs
+++ src/components/input.rs
@@ -6,7 +6,19 @@ pub struct InputProps {
     pub with_icon: Option<AttrValue>,
 
     #[prop_or_default]
+    pub id: Option<AttrValue>,
+
+    #[prop_or_default]
     pub placeholder: Option<AttrValue>,
+
+    #[prop_or_default]
+    pub input_ref: NodeRef,
+
+    #[prop_or_default]
+    pub field_classes: Classes,
+
+    #[prop_or_default]
+    pub wrapper_classes: Classes,
 }
 
 #[function_component(Input)]
@@ -14,11 +26,15 @@ pub fn input(props: &InputProps) -> Html {
     let InputProps {
         with_icon,
         placeholder,
+        id,
+        input_ref,
+        wrapper_classes,
+        field_classes,
     } = props;
 
     html! {
-        <div class={classes!("input")}>
-            <input class={classes!("input__field")} {placeholder} />
+        <div class={classes!(wrapper_classes.clone(), "input")}>
+            <input ref={input_ref.clone()} class={classes!(field_classes.clone(), "input__field")} {placeholder} {id} />
 
             if let Some(icon_name) = with_icon {
                 <span class={classes!(icon_name)}></span>
